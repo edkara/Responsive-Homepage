1df4b4d71b1bc9b90e189c3631d7c03f6f54825a
edkara <eddikaraasenov@gmail.com>
2022-03-19 14:50:14 +0100

    small changes

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 function unfold_nav_item() {
-    alert("HAAAAALOOOOOOOOOO!");
+  alert("HAAAAALOOOOOOOOOO!");
 }
 
 function unfold_note() {
-    $("#note").click(function() {
-        $(this).toggleClass("open");
-    });
+  $("#note").click(function () {
+    $(this).toggleClass("open");
+  });
 }
 
 function sticked() {
-    let note = document.getElementById("note");
-    let notePos = note.getBoundingClientRect().bottom();
-    window.addEventListener("scroll", e => {
-        scrollPos = window.scrollY;
-        if (scrollPos < notePos) {
-
-        }
-    })
+  let note = document.getElementById("note");
+  let notePos = note.getBoundingClientRect().bottom();
+  window.addEventListener("scroll", (e) => {
+    scrollPos = window.scrollY;
+    if (scrollPos < notePos) {
+    }
+  });
 }
 
-$(document).ready(function() {
-    unfold_note();
-    // remove_logo_font();
+$(document).ready(function () {
+  unfold_note();
+  // remove_logo_font();
 });
