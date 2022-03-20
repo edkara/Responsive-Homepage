7d3b70db807618f55f246470f52fd429fa1f67bb
edkara <eddikaraasenov@gmail.com>
2022-03-20 22:31:03 +0100

    Feat(Sections): added credits, partner, home, comm.

--- script.js
+++ script.js
@@ -29,8 +29,42 @@ function search() {
   };
 }
 
+function swiper() {
+  var swiper = new Swiper(".home-slider", {
+    loop: true, 
+    grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+function swiperLogos() {
+  var swiper = new Swiper(".clients-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 $(document).ready(function () {
   unfold_note();
   search();
-  // remove_logo_font();
+  swiper();
+  swiperLogos();
 });
